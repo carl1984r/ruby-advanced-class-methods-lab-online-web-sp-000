ed2c4dbbc43a694f9f0b58b482fa1e32229f0d05
Bry Cox <carl.cc@gmail.com>
2019-02-18 19:04:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   def self.new_from_filename(arg)
     var = arg.split(%r{\W\-\W|.mp3})
     new.tap {|a| a.name = var[1], a.artist_name = var[0]}
-  
+
   end
 
   def self.destroy_all
