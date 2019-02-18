b4c77a7348480cc7c4e91e0a6963f9fbacf4357a
Bry Cox <carl.cc@gmail.com>
2019-02-18 18:43:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
+
+  def initialize
+    @name = name
+    @artist_name = artist_name
+  end
 
   def self.all
     @@all
@@ -8,6 +15,37 @@ class Song
 
   def save
     self.class.all << self
+  end
+  def self.create
+    new.tap {|a| a.save}
+  end
+
+  def self.new_by_name(song)
+    new.tap {|a| a.name = song}
+  end
+
+  def self.create_by_name(song)
+    new.tap {|a| a.save && a.name = song}
+  end
+
+  def self.find_by_name(song)
+    @@all.detect {|x| x if x.name == song}
+  end
+
+  def self.find_or_create_by_name(title)
+    self.find_by_name(title) || self.create_by_name(title)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|x| x.name}
+  end
+
+  def self.new_from_filename(arg)
+
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
